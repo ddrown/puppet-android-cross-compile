47178c89ac2ee941c2e8958f1070183d31950f86
Daniel Drown <dan-android@drown.org>
2014-01-04 17:00:16 -0600

    re-organization into classes, add common packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -16,38 +16,13 @@ define gitrepo($url) {
   }
 }
 
-node default {
-  package {
-    "vim-enhanced": ensure => present;
-    "git": ensure => present;
-  }
-  user {
-    "admin": ensure => present;
-  }
-  file {
-    "/home/admin": ensure => directory, require => User["admin"], owner => "admin";
-    "/home/admin/droid": ensure => directory, require => File["/home/admin"];
-    "/home/admin/droid/lib": ensure => directory, require => File["/home/admin/droid"];
-    "/home/admin/droid/include": ensure => directory, require => File["/home/admin/droid"];
-    "/home/admin/tmp": ensure => directory, require => File["/home/admin"];
-  }
-  gitrepo {
-    "/home/admin/droid/bin":
-      url => "https://github.com/ddrown/android-ports-tools.git",
-      require => File["/home/admin/droid"];
-  }
-
-  # settings - NDK version and gcc version
-  $ndk_version = "r9c"
-  $gcc_version = "4.6"
-
+class android_ndk_install($ndk_version) {
   exec {
     "download-android-ndk":
 # TODO url = http://dl.google.com/android/ndk/android-ndk-$ndk_version-linux-x86_64.tar.bz2",
       command => "/usr/bin/wget http://sandfish.lan/abob/android/android-ndk-$ndk_version-linux-x86_64.tar.bz2",
       cwd => "/home/admin/droid",
-      creates => "/home/admin/droid/android-ndk-$ndk_version-linux-x86_64.tar.bz2",
-      require => File["/home/admin/droid"];
+      creates => "/home/admin/droid/android-ndk-$ndk_version-linux-x86_64.tar.bz2";
     "extract-android-ndk":
       command => "/bin/tar jxf /home/admin/droid/android-ndk-$ndk_version-linux-x86_64.tar.bz2",
       cwd => "/home/admin/droid",
@@ -58,18 +33,70 @@ node default {
     "/home/admin/droid/android-ndk":
       ensure => "/home/admin/droid/android-ndk-$ndk_version",
       require => Exec["extract-android-ndk"];
+  }
+}
+
+class android_ndk_symlinks($gcc_version) {
+  file {
     "/home/admin/droid/android-ndk/default-toolchain":
       ensure => "toolchains/arm-linux-androideabi-$gcc_version/prebuilt/linux-x86_64/",
       require => File["/home/admin/droid/android-ndk"];
     "/home/admin/droid/lib/libgcc.a":
       ensure => "../android-ndk/default-toolchain/lib/gcc/arm-linux-androideabi/$gcc_version/libgcc.a",
-      require => [File["/home/admin/droid/android-ndk/default-toolchain"],File["/home/admin/droid/lib"]];
+      require => File["/home/admin/droid/android-ndk/default-toolchain"];
     "/home/admin/droid/lib/libstdc++":
       ensure => "../android-ndk/sources/cxx-stl/gnu-libstdc++/$gcc_version/",
-      require => [File["/home/admin/droid/android-ndk/default-toolchain"],File["/home/admin/droid/lib"]];
+      require => File["/home/admin/droid/android-ndk/default-toolchain"];
+  }
+}
+
+class android_ndk {
+  # settings - NDK version and gcc version
+  $ndk_version = "r9c"
+  $gcc_version = "4.6"
+
+  class {
+    "android_ndk_install": 
+      ndk_version => $ndk_version,
+      require => Class["admin_user"];
+    "android_ndk_symlinks": 
+      gcc_version => $gcc_version,
+      require => Class["android_ndk_install"];
+  }
+
+}
+
+class admin_user {
+  user {
+    "admin": ensure => present;
+  }
+  file {
+    "/home/admin": ensure => directory, require => User["admin"], owner => "admin";
+    "/home/admin/droid": ensure => directory, require => File["/home/admin"];
+    "/home/admin/droid/lib": ensure => directory, require => File["/home/admin/droid"];
+    "/home/admin/droid/include": ensure => directory, require => File["/home/admin/droid"];
+    "/home/admin/tmp": ensure => directory, require => File["/home/admin"];
   }
   puppetfile {
     "/home/admin/shell":
       mode => 0755;
   }
+}
+
+node default {
+  package {
+    "vim-enhanced": ensure => present;
+    "git": ensure => present;
+    "autoconf": ensure => present;
+    "automake": ensure => present;
+    "mercurial": ensure => present;
+  }
+  gitrepo {
+    "/home/admin/droid/bin":
+      url => "https://github.com/ddrown/android-ports-tools.git",
+      require => File["/home/admin/droid"];
+  }
+
+  include admin_user
+  include android_ndk
 }
